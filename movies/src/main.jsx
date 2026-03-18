19d68c2c96d605ac17a42655e53a25038d00650a
Josh <joshluker99@gmail.com>
2026-03-18 16:20:28 +0000

    Added new review support

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -9,13 +9,15 @@ import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
+
 
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 360000,
-      refetchInterval: 360000, 
+      refetchInterval: 360000,
       refetchOnWindowFocus: false
     },
   },
@@ -31,10 +33,12 @@ const App = () => {
         <MoviesContextProvider>
           <Routes>
             <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-            <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+            <Route path="/reviews/:id" element={<MovieReviewPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
-            <Route path="*" element={ <Navigate to="/" /> } />
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
+
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
@@ -45,5 +49,5 @@ const App = () => {
 
 
 
-const rootElement = createRoot( document.getElementById("root") )
+const rootElement = createRoot(document.getElementById("root"))
 rootElement.render(<App />);
